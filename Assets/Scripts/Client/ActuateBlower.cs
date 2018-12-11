b109c77dd43c4477bcd74770e2b6e65cace76c19
iibanezCode <Ii13022002>
2018-12-11 21:24:27 +0100

    Balls Added

---
+++ Assets/Scripts/Client/ActuateBlower.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActuateBlower : MonoBehaviour {
+
+    public GameObject blower;
+
+	public void blowBlower()
+    {
+        blower.GetComponent<Animator>().SetTrigger("Blow");
+    }
+}
